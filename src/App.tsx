e4c9ff0d73146daa3c058f1216ade7a6dbe66754
mausio <robin.schwenzfeier@icloud.com>
2024-10-03 14:06:40 +0200

    added routing and styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
-import { useState } from 'react'
-import logo from "./assets/theo_info_web_logo.png"
-import './App.css'
 
 function App() {
-
-  return (
-    <>
-          <img src={logo} style={{maxWidth: "100%"}} />
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
---
+++ src/routes/routes.ts
@@ -0,0 +1,29 @@
+export const APP_ROUTES = {
+  home: '/',
+  imprint: 'imprint',
+  sorting: {
+    insertion: 'insertion',
+    merge: 'merge',
+    heap: 'heap',
+    quick: 'quick',
+    radix: 'radix',
+    counting: 'counting',
+    bucket: 'bucket',
+  },
+  search: {
+    linear: 'linear',
+    binary: 'binary',
+  },
+  graphs: {
+    bfs: 'bfs',
+    dfs: 'dfs',
+    dijkstra: 'dijkstra',
+  },
+  coding: {
+    huffmann: 'huffmann',
+  },
+  account: {
+    login: 'login',
+    logout: 'logout',
+  },
+};
